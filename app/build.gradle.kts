b0f7ec911f66da51a7dc0c0f35c043aa95363903
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-04-25 16:25:24 +0700

    refactor: Menambahkan Depedencies Baru

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Custom Dependencies
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
 }
