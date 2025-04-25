c253f207ac7cc0cf48b40e79246acc7e37817d39
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2025-04-26 00:12:15 +0700

    style: Ubah ke warna pilihan
    - Warna
    - fragment
    - nav bottom

--- app/src/main/java/com/example/toolmate/ListAppAdapter.kt
+++ app/src/main/java/com/example/toolmate/ListAppAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
@@ -26,6 +27,9 @@ class ListAppAdapter(private val listapp: ArrayList<apps>): RecyclerView.Adapter
             .load(img)
             .into(holder.imgapp)
         holder.tvName.text = name
+        holder.itemView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, listapp[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int = listapp.size
--- app/src/main/java/com/example/toolmate/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/toolmate/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.toolmate.ListAppAdapter
 import com.example.toolmate.R
@@ -43,6 +39,7 @@ class DashboardFragment : Fragment() {
         rvApps = binding.rvApps
         rvApps.setHasFixedSize(true)
 
+        list.clear()
         list.addAll(getListApps())
         showRecycleList()
     }
